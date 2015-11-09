9c80b66f0cdac14302638ce571b02bc70d76e3d2
aligos <artasikindonesia@gmail.com>
2015-11-09 12:47:01 +0700

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -11,13 +11,13 @@ router.post('/send', function(req, res, next){
 	var transporter = nodemailer.createTransport({
 		service: 'Gmail',
 		auth: {
-			user: 'artasikindonesia@gmail.com',
-			pass: 'aligos555'
+			user: 'yourgmail@gmail.com',
+			pass: 'yourpassword'
 		}
 	});
 	var mailOptions = {
 		from: req.body.email,
-		to: 'artasikindonesia@gmail.com',
+		to: 'yourgmail@gmail.com',
 		subject: 'Website Submission',
 		text: 'You have a new submission with the following details .. Name: '+req.body.name+'Email: '+req.body.email+'Pesan: '+req.body.message,
 		html: '<p>You got new submission with the following details ..</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Pesan: '+req.body.message+'</li></ul>'
